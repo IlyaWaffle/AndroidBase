e0c28f0bbe85d822f2671a1d38eda41b83d8d532
Ilya <krotov891@gmail.com>
2022-08-26 09:48:57 +0300

    Update to multiplatfrom

--- MultiplatformMaps/shared/build.gradle.kts
+++ MultiplatformMaps/shared/build.gradle.kts
@@ -56,3 +56,33 @@ android {
         targetSdk = 32
     }
 }
+
+dependencies {
+
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("com.yandex.android:maps.mobile:4.1.0-lite")
+
+    val room_version = "2.4.3"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    // optional - RxJava3 support for Room
+    implementation("androidx.room:room-rxjava3:$room_version")
+
+    // optional - Guava support for Room, including Optional and ListenableFuture
+    implementation("androidx.room:room-guava:$room_version")
+
+    // optional - Test helpers
+    testImplementation("androidx.room:room-testing:$room_version")
+
+    // optional - Paging 3 Integration
+    implementation("androidx.room:room-paging:2.5.0-alpha02")
+
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("io.ktor:ktor-server-netty:1.3.1")
+
+    implementation("com.just-ai.jaicf:core:1.2.4")
+    implementation("com.just-ai.jaicf:yandex-alice:1.2.4")
+}
